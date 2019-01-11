76e5928eca9e40850a7839e408e84c75b9b3218b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-01-11 17:58:00 -0500

    Populate lowdb

--- geodiff/src/App.js
+++ geodiff/src/App.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Partners } from './Partners'
 import { Map } from './Map'
+import low from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
 
 class App extends Component {
+  _db = null
+  state = {
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    this.intializeDB()
+  }
+
+  intializeDB = () => {
+    const adapter = new LocalStorage('db')
+    this._db = low(adapter)
+
+    const url = new URL(`/db.json`, document.location.origin)
+    fetch(url)
+      .then(response => response.json())
+      .then(({ features }) => {
+        this._db.setState({ features }).write()
+      })
+      .then(() => this.setState({ isLoaded: true }))
+  }
+
   render() {
     return (
       <Main>
-        <PartnerPane>
-          <Partners />
-        </PartnerPane>
-        <MapPane>
-          <Map />
-        </MapPane>
+        {this.state.isLoaded ? (
+          <>
+            <PartnerPane>
+              <Partners />
+            </PartnerPane>
+            <MapPane>
+              <Map />
+            </MapPane>
+          </>
+        ) : (
+          <span>loading...</span>
+        )}
       </Main>
     )
   }
