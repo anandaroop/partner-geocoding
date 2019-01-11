bf0d18d5ff9c226197d4ae9d2330c95268d92e99
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-01-11 16:50:27 -0500

    Scaffold a layout

--- geodiff/src/App.js
+++ geodiff/src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Partners } from './Partners'
+import { Map } from './Map'
 
 class App extends Component {
   render() {
-    return <div>hi</div>
+    return (
+      <Main>
+        <PartnerPane>
+          <Partners />
+        </PartnerPane>
+        <MapPane>
+          <Map />
+        </MapPane>
+      </Main>
+    )
   }
 }
 
 export default App
+
+const Main = styled.main`
+  display: flex;
+  height: 100vh;
+`
+
+const PartnerPane = styled.div`
+  flex: 1 0 60%;
+  max-width: 800px;
+`
+
+const MapPane = styled.div`
+  flex: 1 0 40%;
+  background: #eee;
+`
