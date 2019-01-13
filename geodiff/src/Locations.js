16e86f49e5dd1bab37189610195806e48ee0f3a2
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-01-12 23:15:13 -0500

    Add keyboard navigation

--- geodiff/src/Locations.js
+++ geodiff/src/Locations.js
@@ -1,67 +1,125 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+import _ from 'lodash'
 
 const currentAddress = loc => {
   const { address, address2 } = loc.properties.attributes
   return [address, address2].filter(x => x).join(', ')
 }
 
-export const Locations = ({
-  locations,
-  query,
-  updateQuery,
-  selectLocation,
-  selectedId
-}) => {
-  return (
-    <>
-      <Search>
-        <Input
-          type="search"
-          placeholder="Filter locations by keyword"
-          value={query}
-          onChange={e => updateQuery(e.target.value)}
-        />
-      </Search>
-      <Results>
-        <LocationTable>
-          <thead>
-            <tr>
-              <TH>Name</TH>
-              <TH>Address</TH>
-              <TH>City</TH>
-              <TH>Δ1</TH>
-              <TH>Δ2</TH>
-            </tr>
-          </thead>
-          <tbody>
-            {locations.map(loc => (
-              <TR
-                key={loc.properties.attributes._id}
-                current={loc.properties.attributes._id === selectedId}
-                onClick={e => {
-                  e.preventDefault()
-                  selectLocation(loc.properties.attributes._id)
-                }}
-              >
-                <TD className="name">{loc.properties.partner_name}</TD>
-                <TD className="address">{currentAddress(loc)}</TD>
-                <TD className="city">
-                  <TK />
-                </TD>
-                <TD className="redo">
-                  <TK />
-                </TD>
-                <TD className="scrape">
-                  <TK />
-                </TD>
-              </TR>
-            ))}
-          </tbody>
-        </LocationTable>
-      </Results>
-    </>
-  )
+export class Locations extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleKeyUp)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyUp)
+  }
+
+  handleKeyUp = e => {
+    // ignore if focused in search field
+    if (document.activeElement.type !== 'search') {
+      if (e.key === '/') {
+        document.querySelector('input[type="search"]').focus()
+      }
+      // ignore arrows if focused in map
+      if (!document.activeElement.classList.contains('leaflet-container')) {
+        if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+          const { selectedId, locations, selectLocation } = this.props
+          // find current index
+          const currentLocation = _.find(
+            locations,
+            loc => loc.properties.attributes._id === selectedId
+          )
+          const currentIndex = locations.indexOf(currentLocation)
+          // increment or decrement to find next location
+          const delta = e.key === 'ArrowLeft' ? -1 : +1
+          const newIndex = _.clamp(
+            currentIndex + delta,
+            0,
+            locations.length - 1
+          )
+          const newLocation = locations[newIndex]
+          const newId = newLocation.properties.attributes._id
+          // update app state and browser focus˝
+          selectLocation(newId)
+          document.querySelector(`[data-location-id="${newId}"]`).focus()
+        }
+      }
+    }
+  }
+
+  render() {
+    const {
+      locations,
+      query,
+      updateQuery,
+      selectLocation,
+      selectedId
+    } = this.props
+
+    return (
+      <>
+        <Search>
+          <Input
+            type="search"
+            placeholder="Filter locations by keyword"
+            value={query}
+            onChange={e => updateQuery(e.target.value)}
+          />
+        </Search>
+        <Results>
+          <LocationTable>
+            <thead>
+              <tr>
+                <TH>Name</TH>
+                <TH>Address</TH>
+                <TH>City</TH>
+                <TH>Δ1</TH>
+                <TH>Δ2</TH>
+              </tr>
+            </thead>
+            <tbody>
+              {locations.map(loc => (
+                <TR
+                  key={loc.properties.attributes._id}
+                  current={loc.properties.attributes._id === selectedId}
+                  onClick={e => {
+                    e.preventDefault()
+                    selectLocation(loc.properties.attributes._id)
+                  }}
+                >
+                  <TD className="name">{loc.properties.partner_name}</TD>
+                  <TD
+                    data-location-id={loc.properties.attributes._id}
+                    tabIndex={0}
+                    className="address"
+                    onKeyDown={e => {
+                      if (e.key === 'Enter') {
+                        e.preventDefault()
+                        selectLocation(loc.properties.attributes._id)
+                      }
+                    }}
+                  >
+                    {currentAddress(loc)}
+                  </TD>
+                  <TD className="city">
+                    <TK />
+                  </TD>
+                  <TD className="redo">
+                    <TK />
+                  </TD>
+                  <TD className="scrape">
+                    <TK />
+                  </TD>
+                </TR>
+              ))}
+            </tbody>
+          </LocationTable>
+        </Results>
+      </>
+    )
+  }
 }
 
 const Search = styled.div`
@@ -85,7 +143,8 @@ const Input = styled.input`
 
 const LocationTable = styled.table`
   border-collapse: collapse;
-  width: 100%;
+  width: calc(100% - 2rem);
+  margin: auto;
 `
 
 const TR = styled.tr`
