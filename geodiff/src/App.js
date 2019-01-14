718d08db1a6b618f3db71b5b54ca0ed8f243b144
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-01-13 21:55:11 -0500

    Build out sorts and filters

--- geodiff/src/App.js
+++ geodiff/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import _ from 'lodash'
 import { Locations } from './Locations'
 import { Map } from './Map'
 import { getDatabase } from './db.js'
 
-const SORTS = {
-  NAME: 'NAME'
+export const SORTS = {
+  NAME: 'NAME',
+  CITY: 'CITY',
+  DELTA_1: 'DELTA_1',
+  DELTA_2: 'DELTA_2'
 }
 
 class App extends Component {
@@ -13,9 +17,10 @@ class App extends Component {
 
   state = {
     isLoaded: false,
-    sort: SORTS.NAME,
+    scraped: false,
     query: '',
-    selectedId: null
+    selectedId: null,
+    sort: SORTS.NAME
   }
 
   componentDidMount() {
@@ -33,18 +38,38 @@ class App extends Component {
     switch (this.state.sort) {
       case SORTS.NAME:
         return 'properties.partner_name'
+      case SORTS.CITY:
+        return ['properties.assigned_city', 'properties.partner_name']
+      case SORTS.DELTA_1:
+        return loc =>
+          -1 * _.get(loc, 'properties.geocodes.redo_old_address.distance')
+      case SORTS.DELTA_2:
+        return loc =>
+          -1 * _.get(loc, 'properties.geocodes.new_address.distance')
       default:
         return 'properties.partner_name'
     }
   }
 
-  getFilterCriteria = () => {
+  getQueryCriteria = () => {
     const query = this.state.query.toLowerCase()
-    const isPresent = query.trim() !== ''
-    const predicate = isPresent
-      ? loc => loc.properties.partner_name.toLowerCase().includes(query)
-      : () => true
-    return predicate
+    const isQueryPresent = query.trim() !== ''
+    const fieldsToSearch = [
+      'properties.partner_name',
+      'properties.assigned_city'
+    ]
+
+    if (!isQueryPresent) return () => true
+
+    return loc =>
+      fieldsToSearch.some(f => {
+        const val = _.get(loc, f)
+        return val && val.toLowerCase().includes(query)
+      })
+  }
+
+  getScrapedCriteria = () => {
+    return this.state.scraped ? 'properties.new_address_string' : () => true
   }
 
   updateQuery = val => {
@@ -59,17 +84,30 @@ class App extends Component {
     })
   }
 
+  updateSort = sort => {
+    this.setState({
+      sort
+    })
+  }
+
+  updateScraped = scraped => {
+    this.setState({
+      scraped
+    })
+  }
+
   render() {
     if (!this.state.isLoaded) {
       return <span>loading...</span>
     }
 
-    const { query, selectedId } = this.state
+    const { query, selectedId, sort, scraped } = this.state
 
     const locations = this._db
       .get('features')
+      .filter(this.getQueryCriteria())
+      .filter(this.getScrapedCriteria())
       .sortBy(this.getSortCriteria())
-      .filter(this.getFilterCriteria())
       .value()
 
     const selectedLocation = selectedId
@@ -84,10 +122,14 @@ class App extends Component {
         <PartnerPane>
           <Locations
             locations={locations}
-            selectedId={selectedId}
             query={query}
-            updateQuery={this.updateQuery}
+            scraped={scraped}
+            selectedId={selectedId}
             selectLocation={this.selectLocation}
+            sort={sort}
+            updateQuery={this.updateQuery}
+            updateScraped={this.updateScraped}
+            updateSort={this.updateSort}
           />
         </PartnerPane>
         <MapPane>
--- geodiff/src/Locations.js
+++ geodiff/src/Locations.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import _ from 'lodash'
 
+import { SORTS } from './App'
+
 const currentAddress = loc => {
   const { address, address2 } = loc.properties.attributes
   return [address, address2].filter(x => x).join(', ')
@@ -53,9 +55,13 @@ export class Locations extends React.Component {
     const {
       locations,
       query,
-      updateQuery,
+      scraped,
+      selectedId,
       selectLocation,
-      selectedId
+      sort,
+      updateQuery,
+      updateScraped,
+      updateSort
     } = this.props
 
     return (
@@ -67,16 +73,92 @@ export class Locations extends React.Component {
             value={query}
             onChange={e => updateQuery(e.target.value)}
           />
+          <Controls>
+            <Group>
+              <Label>
+                <input
+                  type="checkbox"
+                  name="scraped"
+                  checked={scraped}
+                  onChange={e => updateScraped(e.target.checked)}
+                />
+                Scraped only
+              </Label>
+            </Group>
+            <Group>
+              Sort by:
+              <Label>
+                <input
+                  type="radio"
+                  checked={sort === SORTS.NAME}
+                  name="sort"
+                  onChange={() => updateSort(SORTS.NAME)}
+                />
+                name
+              </Label>
+              <Label>
+                <input
+                  type="radio"
+                  checked={sort === SORTS.CITY}
+                  name="sort"
+                  onChange={() => updateSort(SORTS.CITY)}
+                />
+                city
+              </Label>
+              <Label>
+                <input
+                  type="radio"
+                  checked={sort === SORTS.DELTA_1}
+                  name="sort"
+                  onChange={() => updateSort(SORTS.DELTA_1)}
+                />
+                Δ1
+              </Label>
+              <Label>
+                <input
+                  type="radio"
+                  defaultChecked={sort === SORTS.DELTA_2}
+                  name="sort"
+                  onChange={() => updateSort(SORTS.DELTA_2)}
+                />
+                Δ2
+              </Label>
+            </Group>
+          </Controls>
         </Search>
         <Results>
           <LocationTable>
             <thead>
               <tr>
-                <TH>Name</TH>
+                <TH
+                  className={'clickable'}
+                  current={sort === SORTS.NAME}
+                  onClick={() => updateSort(SORTS.NAME)}
+                >
+                  Name
+                </TH>
                 <TH>Address</TH>
-                <TH>City</TH>
-                <TH>Δ1 (m)</TH>
-                <TH>Δ2 (m)</TH>
+                <TH
+                  className={'clickable'}
+                  current={sort === SORTS.CITY}
+                  onClick={() => updateSort(SORTS.CITY)}
+                >
+                  City
+                </TH>
+                <TH
+                  className={'clickable'}
+                  current={sort === SORTS.DELTA_1}
+                  onClick={() => updateSort(SORTS.DELTA_1)}
+                >
+                  Δ1 (m)
+                </TH>
+                <TH
+                  className={'clickable'}
+                  current={sort === SORTS.DELTA_2}
+                  onClick={() => updateSort(SORTS.DELTA_2)}
+                >
+                  Δ2 (m)
+                </TH>
               </tr>
             </thead>
             <tbody>
@@ -135,13 +217,9 @@ export class Locations extends React.Component {
 }
 
 const Search = styled.div`
-  height: 4rem;
+  height: 6rem;
   display: flex;
-`
-
-const Results = styled.div`
-  height: calc(100vh - 4rem);
-  overflow-y: auto;
+  flex-direction: column;
 `
 
 const Input = styled.input`
@@ -151,6 +229,30 @@ const Input = styled.input`
   padding: 0.5rem;
   font-size: 1rem;
   border: solid 1px #ddd;
+`
+
+const Controls = styled.div`
+  width: calc(100% - 2rem);
+  height: 2rem;
+  margin: auto;
+  opacity: 0.5;
+`
+
+const Group = styled.span`
+  margin: 0 1em 0 0;
+`
+
+const Label = styled.label`
+  padding: 0 0.5em;
+
+  input {
+    margin-right: 0.4em;
+  }
+`
+
+const Results = styled.div`
+  height: calc(100vh - 6rem);
+  overflow-y: auto;
 `
 
 const LocationTable = styled.table`
@@ -173,12 +275,16 @@ const TR = styled.tr`
 `
 
 const TH = styled.th`
-  background: #f8f8f8;
+  background: ${p => (p.current ? '#eeddff' : '#f0f0f0')};
   border-left: solid 5px white;
   border-right: solid 5px white;
   padding: 0.5rem;
   position: sticky;
   top: 0;
+
+  &.clickable {
+    cursor: pointer;
+  }
 `
 
 const TD = styled.td`
